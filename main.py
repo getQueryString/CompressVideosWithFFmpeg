f2441d7c27deb7f7e35d96deda8b8d45fe13b6fd
BuildTools <unconfigured@null.spigotmc.org>
2023-01-11 22:59:42 +0100

    Added function

--- main.py
+++ main.py
@@ -2,14 +2,44 @@
 import os
 import subprocess
 
-# Pfad zu den Videos, die komprimiert werden sollen
-input_dir = "C:\\"
+"""
+input_dir = ""
 
-# Pfad zum Speicherort der komprimierten Videos
-output_dir = "C:\\"
+output_dir = ""
+"""
 
 
-def compress():
+# Eigene Idee & Hilfe durch ChatGPT
+def queue():
+    year = 2021
+    month = 6
+    directory = 6
+
+    while year != 2023 and month != 13:
+        # Pfad zu den Videos, die komprimiert werden sollen
+        input_dir = os.path.join("C:\\", "Users", "USER", "Desktop", "compressed", "nicht komprimiert", str(year),
+                                 "{:02d}".format(month), "{:02d}_Videos".format(directory))
+
+        # Pfad zum Speicherort der komprimierten Videos
+        output_dir = os.path.join("C:\\", "Users", "USER", "Desktop", "compressed", "komprimiert", str(year),
+                                  "{:02d}".format(month), "{:02d}_Videos".format(directory))
+
+        print(input_dir)
+        print(output_dir)
+        print()
+
+        month += 1
+        directory += 1
+
+        if month > 12:
+            year += 1
+            month = 1
+            directory = 1
+
+        compress(input_dir, output_dir)
+
+
+def compress(input_dir, output_dir):
     # Überprüfen, ob das Eingabeverzeichnis existiert
     if not os.path.exists(input_dir):
         print("Eingabeverzeichnis nicht gefunden.")
@@ -31,13 +61,14 @@ def compress():
             # Den vollständigen Pfad zum Ausgabevideo erstellen
             output_path = os.path.join(output_dir, file)
             # ffmpeg-Befehl ausführen, um das Video zu komprimieren und die Metadaten zu beibehalten
-            ret_code = subprocess.call(
+            ret_code = subprocess.run(
                 ['ffmpeg', '-i', input_path, '-c:v', 'libx264', '-crf', '22', '-c:a', 'copy', '-vf', 'scale=-2:720',
-                 '-map_metadata', '0', '-y', output_path], stderr=subprocess.STDOUT)
+                 '-map_metadata', '0', '-y', output_path], stderr=subprocess.PIPE)
             if ret_code == 0:
                 count += 1
             else:
-                print("Fehler bei der Komprimierung von " + file)
+                print("Fehler bei der Komprimierung von " + file + ":")
+                print(ret_code.stderr.decode())
         else:
             print("Videoformat nicht unterstützt: " + file)
 
@@ -47,4 +78,5 @@ def compress():
         print("Keine Videos zum Komprimieren gefunden.")
 
 
-compress()
+queue()
+# compress()
